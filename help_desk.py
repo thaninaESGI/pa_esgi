3b2f1d8472eb70bf26a129447d4097955a8030d6
thaninaESGI <thanina.A1996@gmail.com>
2024-06-23 23:16:22 +0200

    update key managment in load_db

--- help_desk.py
+++ help_desk.py
@@ -140,7 +140,7 @@ class HelpDesk():
             if len(distinct sources) == 1:
                 return f"Voici la source qui pourrait t'être utile :  \n- {distinct_sources_str}"
 
-            elif len distinct sources) > 1:
+            elif len (distinct sources) > 1:
                 return f"Voici {len distinct sources)} sources qui pourraient t'être utiles :  \n- {distinct_sources_str}"
 
         return "Je n'ai trouvé pas trouvé de ressource pour répondre à ta question"
