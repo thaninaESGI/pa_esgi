2231dcc20075aabfa950f7759db0b5eb81e83870
thaninaESGI <thanina.A1996@gmail.com>
2024-07-11 16:37:56 +0200

    test

--- test.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-# Vérifiez que la variable d'environnement est correctement chargée
-print(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-
